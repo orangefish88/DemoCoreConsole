b01553f6dc9343adb46565c8dde109fdcb3fff7c
Dave Bear <dave.bear@test.com>
2019-03-22 13:41:02 -0700

    Modified the writeline

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace democonsoleapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! i have installed Git");
         }
     }
 }
